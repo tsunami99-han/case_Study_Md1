b7e72158f9527bdb3086d1bb5131b841b6172e7c
tsunami99-han <doanhongson050999@gmail.com>
2021-04-27 17:10:36 +0700

    sukien

--- plan_and_zombie.js
+++ plan_and_zombie.js
@@ -1,43 +1,58 @@
-Plant = function (_x, _y) {
+Plant = function (_x, _y, _width, _height) {
     this.x = _x;
     this.y = _y;
     this.hp = 5;
+    this.width = _width
+    this.height = _height
     this.dmg = 2;
+    this.width = 100;
+    this.height = 100;
     this.image = new Image();
     this.image.src = 'lv3.png';
-    this.onload1 = function () {
-        let canvas = document.getElementById("screen1");
-        let context = canvas.getContext('2d');
-        context.drawImage(this.image, this.x, this.y,100,100);
+    this.drawPlant = function () {
+        context.drawImage(this.image, this.x, this.y, this.width, this.height);
+    }
+    this.down = function () {
+        if (this.y < 500) {
+            this.y += 100
+        }
+    }
+    this.up = function () {
+        if (this.y > 0) {
+            this.y -= 100
+        }
     }
 }
+
 Zombie = function (_x, _y,) {
     this.x = _x;
     this.y = _y;
-    this.width=100
-    this.height=100
+    this.width = 100
+    this.height = 100
     this.hp = 6;
     this.dmg = 1;
     this.image = new Image()
-    this.image.src = 'zombielv.png'
-    this.speed = _speed
+    this.image.src = 'zombielv' + (Math.round(Math.random() * 2) + 1) + '.png'
+    this.drawZombie = function (context) {
+        context.drawImage(this.image, this.x, this.y, this.width, this.height)
+    }
+    this.move = function () {
+        console.log(this.x)
+        this.x -=5
+    }
 }
 Shoot = function (plant) {
     this.x = plant.x
-    this.y = plant.y
+    this.y = plant.y + 20;
+    this.width = 100;
+    this.height = 30;
     this.dmg = plant.dmg
     this.image = new Image()
     this.image.src = 'bullet.png'
     this.draw = function (context) {
-        context.drawImage(this.image, this.x, this.y,100,50)
+        context.drawImage(this.image, this.x, this.y, 100, 50)
     }
     this.move = function () {
         this.x += 10;
     }
 }
-// function upDate(){
-//     this.dmg+=1
-// }
-// function plantAttack(){
-//
-// }
